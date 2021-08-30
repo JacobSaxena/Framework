773e2aba62b37bbda695a69a0d65de921d062bc4
JacobSaxena <jacobsaxena10@gmail.com>
2021-08-30 13:46:04 +0530

    master

---
+++ src/test/java/Application.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.AfterClass;
+
+public class Application {
+	@org.testng.annotations.Test
+	public void Test() {
+		System.out.println("jacob");
+	}
+	@AfterClass
+	public void Test1() {
+		System.out.println("jacob");
+	}
+
+}
